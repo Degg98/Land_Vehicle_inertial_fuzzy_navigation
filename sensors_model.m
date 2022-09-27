cbda2e2d71589fb816e7962abc63a01cdcc56675
Pietro Gori <p.gori3@studenti.unipi.it>
2022-09-27 10:45:07 +0100

    Finito di scrivere EKF ma Vf diverge

--- sensors_model.m
+++ sensors_model.m
@@ -1,3 +1,22 @@
 %% Sensors Init Parameters
 
-%% Accelerometer
+% Accelerometer
+Ba = 0.02*ones(3,1); % m/s^2
+SFa = 0.05;
+MISa = 0.1; % deg
+stdv_a = 0.0055; % standard deviation
+Wna = stdv_a*randn(3,1); % White Noise
+
+% Gyros
+Bg = 5*ones(3,1); % deg/s
+SFg = 0;
+MISg = 0.1; % deg
+stdv_g = 0.6325; % standard deviation
+Wng = stdv_g*randn(3,1); % White Noise
+
+
+%% Init fuzzy identification
+load('FUZZY.mat')
+load('Measure_collection.mat')
+
+n = 0.1;
