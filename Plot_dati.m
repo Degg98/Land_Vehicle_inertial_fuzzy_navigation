0492c78a43f41e3445698695f8efbdf039692b6d
Pietro Gori <p.gori3@studenti.unipi.it>
2022-10-04 11:45:01 +0100

    Inizio traiettoria circuito
    Da riguardare la logica fuzzy per il SoM

--- Plot_dati.m
+++ Plot_dati.m
@@ -2,14 +2,16 @@
 n = length(out.tout);
 % Estraggo le componenti di posizione
 j = 1;
-for i=1:100:n
+for i=1:n
 % x_plot(j) = out.x.signals.values(1,1,i);
 % y_plot(j) = out.y.signals.values(1,1,i);
 % z_plot(j) = out.z.signals.values(1,1,i);
-xd_plot(j) = out.xd.signals.values(i);
-yd_plot(j) = out.yd.signals.values(i);
+xd_plot(j) = out.real_pos.signals.values(1,1,i);
+yd_plot(j) = out.real_pos.signals.values(2,1,i);
+zd_plot(i) = out.real_pos.signals.values(3,1,i);
 j = j + 1;
 end
 
 % Plot delle posizioni del drone
-plot(xd_plot, yd_plot)
+plot3(xd_plot, yd_plot, zd_plot)
+axis equal
