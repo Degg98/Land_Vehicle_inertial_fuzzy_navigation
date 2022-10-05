d17ee0c9bc18d1d25140e7b52e16703591dcae2a
Pietro Gori <p.gori3@studenti.unipi.it>
2022-10-05 15:49:36 +0100

    Aggiunta compensazione gravità dell'accelerometro

--- Plot_dati.m
+++ Plot_dati.m
@@ -1,11 +1,11 @@
 %% PLOT DATI
-n = length(out.tout);
+s = length(out.tout);
 % Estraggo le componenti di posizione
 j = 1;
-for i=1:n
-% x_plot(j) = out.x.signals.values(1,1,i);
-% y_plot(j) = out.y.signals.values(1,1,i);
-% z_plot(j) = out.z.signals.values(1,1,i);
+for i=1:s
+x_plot(j) = out.x.signals.values(1,1,i);
+y_plot(j) = out.y.signals.values(1,1,i);
+z_plot(j) = out.z.signals.values(1,1,i);
 xd_plot(j) = out.real_pos.signals.values(1,1,i);
 yd_plot(j) = out.real_pos.signals.values(2,1,i);
 zd_plot(i) = out.real_pos.signals.values(3,1,i);
@@ -13,5 +13,5 @@ j = j + 1;
 end
 
 % Plot delle posizioni del drone
-plot3(xd_plot, yd_plot, zd_plot)
+plot3(x, y, z, xd_plot, yd_plot, zd_plot)
 axis equal
